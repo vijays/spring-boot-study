4d69d749ee0f283bd839ebb7c322a350a24b5147
vijays <vijays@users.noreply.github.com>
2022-09-07 14:28:18 +0530

    Created MyController and mapped home URL

---
+++ spring-boot-study/src/main/java/com/vijays/springbootstudy/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.vijays.springbootstudy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping ("/home")
+	public String home() {
+		return "Welcome to home page!" ;
+	}
+
+}
