623b5a99a7e0537d60caea588dfa5ed369235e07
vijays <vijays@users.noreply.github.com>
2022-09-07 15:15:28 +0530

    Created CourseService

--- spring-boot-study/src/main/java/com/vijays/springbootstudy/controllers/MyController.java
+++ spring-boot-study/src/main/java/com/vijays/springbootstudy/controllers/MyController.java
@@ -1,7 +1,11 @@
 package com.vijays.springbootstudy.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vijays.springbootstudy.entities.Course;
 
 @RestController
 public class MyController {
@@ -11,4 +15,8 @@ public class MyController {
 		return "Welcome to home page!" ;
 	}
 
+	@GetMapping ("/courses")
+	public List<Course> getCourses() {
+		
+	}
 }
