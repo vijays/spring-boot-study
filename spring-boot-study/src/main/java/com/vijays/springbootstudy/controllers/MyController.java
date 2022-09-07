2bc4e0870d1632be53750b777e467aa2d7239bad
vijays <vijays@users.noreply.github.com>
2022-09-07 14:57:14 +0530

    Create Course entity

--- spring-boot-study/src/main/java/com/vijays/springbootstudy/controllers/MyController.java
+++ spring-boot-study/src/main/java/com/vijays/springbootstudy/controllers/MyController.java
@@ -1,4 +1,4 @@
-package com.vijays.springbootstudy.controller;
+package com.vijays.springbootstudy.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
