a9f154aef5cf8f351c9012d9b194c6fe0ed827dd
vijays <vijays@users.noreply.github.com>
2022-09-07 15:30:14 +0530

    Implemented service to get courses

--- spring-boot-study/src/main/java/com/vijays/springbootstudy/controllers/MyController.java
+++ spring-boot-study/src/main/java/com/vijays/springbootstudy/controllers/MyController.java
@@ -2,13 +2,18 @@ package com.vijays.springbootstudy.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vijays.springbootstudy.entities.Course;
+import com.vijays.springbootstudy.services.CourseService;
 
 @RestController
 public class MyController {
+	
+	@Autowired
+	public CourseService courses ;
 	
 	@GetMapping ("/home")
 	public String home() {
@@ -17,6 +22,6 @@ public class MyController {
 
 	@GetMapping ("/courses")
 	public List<Course> getCourses() {
-		
+		return this.courses.getCourses() ;
 	}
 }
